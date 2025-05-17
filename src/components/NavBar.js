1e5d6449bb933c7435dd0fcf99984162c69447d3
“Ivona” <ivona.rieger@gmail.com>
2025-05-17 18:52:30 +0200

    fixed navbar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,13 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/logo.png";
 
 const NavBar = () => {
-  return (
+    return (
     <Navbar expand="md" fixed="top">
-      <Container fluid className="px-0">
+      <Container fluid className="px-3">
         <Navbar.Brand className="ps-3"> <img src={logo} alt="logo" height="35" /> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto pe-3">
+          <Nav className="ps-3 pe-3 ms-md-auto text-start">
             <Nav.Link>
                 <i class="bi bi-house"></i>{' '}
                 Home
@@ -27,6 +27,6 @@ const NavBar = () => {
       </Container>
     </Navbar>
   );
-};
+}
 
 export default NavBar;
