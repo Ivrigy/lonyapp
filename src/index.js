73d560a3156d6d3e820822e2c225eba7a9560013
“Ivona” <ivona.rieger@gmail.com>
2025-05-17 18:11:26 +0200

    add and update style and favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
