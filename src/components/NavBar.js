afd600acc1c1614371bdf466463ee266ac968178
“Ivona” <ivona.rieger@gmail.com>
2025-05-17 18:03:15 +0200

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container fluid className="px-0">
+        <Navbar.Brand className="ps-3"> <img src={logo} alt="logo" height="35" /> </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link>
+                <i class="bi bi-house"></i>{' '}
+                Home
+                </Nav.Link>
+            <Nav.Link>
+                <i class="bi bi-box-arrow-in-right"></i>{' '}
+                Sign in
+                </Nav.Link>
+            <Nav.Link>
+                <i class="bi bi-door-open"></i>{' '}
+                Sign up
+                </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
