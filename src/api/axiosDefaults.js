f13f1ab195b32bbb0b20b0e37c0037887e5b6369
“Ivona” <ivona.rieger@gmail.com>
2025-05-26 13:24:48 +0200

    add refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://lony-api-3e20bf0b1e37.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
