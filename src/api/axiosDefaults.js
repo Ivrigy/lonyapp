059c198f8f9fe08f885ad9b38e78918faabad9ff
“Ivona” <ivona.rieger@gmail.com>
2025-05-27 14:59:40 +0200

    update axios defaults to use axios req

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://lony-api-3e20bf0b1e37.herokuapp.com/";
-axios.defaults.withCredentials = true;
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+export const axiosReq = axios.create({
+  baseURL: "https://lony-api-3e20bf0b1e37.herokuapp.com/",
+  withCredentials: true,
+});
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+export const axiosRes = axios.create({
+  baseURL: "https://lony-api-3e20bf0b1e37.herokuapp.com/",
+  withCredentials: true,
+});
 
