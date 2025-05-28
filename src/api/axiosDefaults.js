42de0563b3faa99873cfe5b6e8b3820e4f56312a
“Ivona” <ivona.rieger@gmail.com>
2025-05-28 23:41:19 +0200

    update axios for csrf token

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,10 @@
-// src/api/axiosDefaults.js
 import axios from "axios";
 
 const baseConfig = {
   baseURL: "https://lony-api-3e20bf0b1e37.herokuapp.com/",
   withCredentials: true,
+  xsrfCookieName: "csrftoken",    // <— read this cookie
+  xsrfHeaderName: "X-CSRFToken",  // <— set this header
 };
 
 export const axiosReq = axios.create(baseConfig);
