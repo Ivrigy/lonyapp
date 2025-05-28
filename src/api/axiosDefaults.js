2142c624cfc40f260433bfa1241c4033c75b5ae5
“Ivona” <ivona.rieger@gmail.com>
2025-05-28 19:34:07 +0200

    try to point at api

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://lony-api-3e20bf0b1e37.herokuapp.com/";
+axios.defaults.baseURL = "https://lony-api-3e20bf0b1e37.herokuapp.com/api/";
 axios.defaults.withCredentials = true;
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
