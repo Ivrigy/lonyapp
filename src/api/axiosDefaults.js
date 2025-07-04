96fd43e9fb53d9615864224a5c2114e15c589e93
“Ivona” <ivona.rieger@gmail.com>
2025-07-04 15:22:00 +0200

    update for new backend

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,49 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://lony-api-3e20bf0b1e37.herokuapp.com/",
-});
+axios.defaults.baseURL =  "https://lony-backend-86431739f0ea.herokuapp.com/",
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-api.interceptors.response.use(
-  response => response,
-  async error => {
-    const origReq = error.config;
-    const url = origReq.url || "";
-
-    if (
-      url.endsWith("/dj-rest-auth/login/") ||
-      url.endsWith("/dj-rest-auth/registration/") ||
-      url.endsWith("/dj-rest-auth/token/refresh/") ||
-      url.endsWith("/dj-rest-auth/user/")
-    ) {
-      return Promise.reject(error);
-    }
-
-    if (error.response?.status === 401 && !origReq._retry) {
-      origReq._retry = true;
-      const refresh = localStorage.getItem("refresh_token");
-      if (refresh) {
-        try {
-          const { data } = await api.post("/dj-rest-auth/token/refresh/", { refresh });
-          localStorage.setItem("access_token", data.access);
-          origReq.headers.Authorization = `Bearer ${data.access}`;
-          return api(origReq);
-        } catch {}
-      }
-    }
-
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
-    return Promise.reject(error);
-  }
-);
-
-export default api;
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
