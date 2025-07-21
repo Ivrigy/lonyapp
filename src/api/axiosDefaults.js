894c2ab202c3e0a4b3609ef76230ae33888d922d
“Ivona” <ivona.rieger@gmail.com>
2025-07-21 19:48:56 +0200

    add access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,6 @@ axios.defaults.baseURL =  "https://lony-backend-86431739f0ea.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
