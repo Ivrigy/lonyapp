fb468ba3a7a7d1441ba79e2769492b2aa0b7c2c3
“Ivona” <ivona.rieger@gmail.com>
2025-07-22 00:37:53 +0200

    add fallback as avatar was not working

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
+const defaultAvatar = "https://res.cloudinary.com/da5x8a1jh/image/upload/v1753130079/default_profile_bbo1dj.jpg";
+
 const Avatar = ({ src, height = 45, text }) => {
   return (
     <span>
       <img
         className={styles.Avatar}
-        src={src}
+        src={src || defaultAvatar}
         height={height}
         width={height}
-        alt="avatar"
+        alt={text || "avatar"}
+        onError={e => {
+          e.target.onerror = null;
+          e.target.src = defaultAvatar;
+        }}
       />
       {text}
     </span>
