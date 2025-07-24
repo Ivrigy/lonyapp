916e5c4c0e7315f57d4d44074a77dc0a57ec573b
“Ivona” <ivona.rieger@gmail.com>
2025-07-25 01:20:03 +0200

    Append default image_filter as serializer is rejecting

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -48,20 +48,23 @@ function PostCreateForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
+
     formData.append("title", title);
     formData.append("content", content);
     formData.append("image", imageInput.current.files[0]);
 
-    try {
-      const { data } = await axiosReq.post("/posts/", formData);
-      history.push(`/posts/${data.id}`);
-    } catch (err) {
-      console.log(err);
-      if (err.response?.status !== 401) {
-        setErrors(err.response?.data);
-      }
+    formData.append("image_filter", "normal");
+
+     try {
+    const { data } = await axiosReq.post("/posts/", formData);
+    history.push(`/posts/${data.id}`);
+  } catch (err) {
+    console.log("Create error:", err.response?.data);
+    if (err.response?.status !== 401) {
+      setErrors(err.response?.data);
     }
-  };
+  }
+};
 
   const textFields = (
     <div className="text-center">
