090f66cc599bab02161bed65e09faaa8328d9212
“Ivona” <ivona.rieger@gmail.com>
2025-07-25 14:05:17 +0200

    fix UI on button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
