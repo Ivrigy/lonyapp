a6666e459661d8ecbf2dd317388b54464a61630f
“Ivona” <ivona.rieger@gmail.com>
2025-07-25 15:55:54 +0200

    add PopularProfiles component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,6 +13,7 @@ import NoResults from "../../assets/no-results.png";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -80,7 +81,7 @@ function PostsPage({ message, filter = "" }) {
       </Col>
 
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+         <PopularProfiles />
       </Col>
     </Row>
   );
