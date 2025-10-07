1225d163e71d5bb5876eb006073bce15f40add22
“Ivona” <ivona.rieger@gmail.com>
2025-10-07 13:53:10 +0200

    add  functionality to unfollow a profile

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -15,9 +15,25 @@ export const fetchMoreData = async (resource, setResource) => {
   } catch (err) {}
 };
 
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? { ...profile, following_count: profile.following_count - 1 }
+    : profile;
+};
+
 export const followHelper = (profile, clickedProfile, following_id) => {
   if (profile.id === clickedProfile.id) {
-    return { ...profile, followers_count: profile.followers_count + 1, following_id };
+    return {
+      ...profile,
+      followers_count: profile.followers_count + 1,
+      following_id,
+    };
   }
   if (profile.is_owner) {
     return { ...profile, following_count: profile.following_count + 1 };
