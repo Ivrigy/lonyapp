a4106b4fbb3e98416c500b7f450535d178b4937f
“Ivona” <ivona.rieger@gmail.com>
2025-10-07 15:49:06 +0200

    add ProfileEditDropdown

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Dropdown } from 'react-bootstrap';
-import styles from '../styles/MoreDropdown.module.css';
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+import { useHistory } from "react-router";
+import styles from "../styles/MoreDropdown.module.css";
 
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
   <i
     className="bi bi-three-dots-vertical"
     ref={ref}
-    onClick={e => {
+    onClick={(e) => {
       e.preventDefault();
       onClick(e);
     }}
@@ -16,8 +17,7 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
 export const MoreDropdown = ({ handleEdit, handleDelete }) => (
   <Dropdown className="ms-auto" drop="start">
     <Dropdown.Toggle as={ThreeDots} />
-
-    <Dropdown.Menu className="text-center" popperConfig={{ strategy: 'fixed' }}>
+    <Dropdown.Menu className="text-center" popperConfig={{ strategy: "fixed" }}>
       <Dropdown.Item
         className={styles.DropdownItem}
         onClick={handleEdit}
@@ -35,3 +35,33 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => (
     </Dropdown.Menu>
   </Dropdown>
 );
+
+export const ProfileEditDropdown = ({ id }) => {
+  const history = useHistory();
+
+  return (
+    <Dropdown className={`ms-auto px-3 ${styles.Absolute}`} drop="start">
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="bi bi-pencil-square" /> edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="bi bi-person-badge" /> change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="bi bi-key" /> change password
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
