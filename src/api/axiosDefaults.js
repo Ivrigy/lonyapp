555487fa3cedd755a9402779256d722ca2deac09
“Ivona” <ivona.rieger@gmail.com>
2025-10-09 16:17:31 +0200

    fix again to axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,28 +1,10 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://lony-backend-86431739f0ea.herokuapp.com/";
+axios.defaults.baseURL =  "https://lony-backend-86431739f0ea.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: true,
-});
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
 
-axiosReq.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const original = error.config;
-    if (!original || original._retry) return Promise.reject(error);
-    if (error.response?.status === 401) {
-      try {
-        original._retry = true;
-        await axiosReq.post("/dj-rest-auth/token/refresh/");
-        return axiosReq(original);
-      } catch (e) {
-        return Promise.reject(e);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
 
-export const axiosRes = axiosReq;
